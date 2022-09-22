a37e5a23ac7ecef966200172481ba3e588872aec
mayuras7685 <mayurasodara@gmail.com>
2022-09-22 23:04:18 +0530

    destination language function

--- app.py
+++ app.py
@@ -62,3 +62,23 @@ def takecommand():
 query = takecommand()
 while (query == "None"):
     query = takecommand()
+
+
+
+#destiation language 
+def destination_language():
+    print("Enter the language in which you want to convert \
+    : Ex. Hindi , English , etc.")
+    print()
+  
+    # Input destination language in which the user 
+    # wants to translate
+    to_lang = takecommand()
+    while (to_lang == "None"):
+        to_lang = takecommand()
+    to_lang = to_lang.lower()
+    return to_lang
+  
+to_lang = destination_language()
+  
+
